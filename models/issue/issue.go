597e37f614c236d1941c9c3155111c7c98a16013
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-17 16:11:30 -0800

    changing bson

--- models/issue/issue.go
+++ models/issue/issue.go
@@ -7,7 +7,7 @@ type Issue struct {
 	ID          bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title       string        `bson:"title" json:"title" binding:"required"`
 	Description string        `bson:"description" json:"description" binding:"required"`
-	Author      bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-	Resolved_By bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
-	City        bson.ObjectId `json:"_id,omitempty" bson:"_id,omitempty"`
+	Author      bson.ObjectId `json:"author_id,omitempty" bson:"author_id,omitempty"`
+	Resolved_By bson.ObjectId `json:"resolved_id,omitempty" bson:"resolved_id,omitempty"`
+	City        bson.ObjectId `json:"city_id,omitempty" bson:"city_id,omitempty"`
 }

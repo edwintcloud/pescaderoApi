3d25a7400728020405587460b9c0eeb548c3acd2
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-18 08:39:03 -0800

    issue model validation 🚁

--- models/issue/issue.go
+++ models/issue/issue.go
@@ -1,6 +1,10 @@
 package issue
 
-import "github.com/globalsign/mgo/bson"
+import (
+	"errors"
+
+	"github.com/globalsign/mgo/bson"
+)
 
 // Issue model
 type Issue struct {
@@ -10,4 +14,20 @@ type Issue struct {
 	Author      bson.ObjectId `json:"author_id,omitempty" bson:"author_id,omitempty"`
 	Resolved_By bson.ObjectId `json:"resolved_id,omitempty" bson:"resolved_id,omitempty"`
 	City        bson.ObjectId `json:"city_id,omitempty" bson:"city_id,omitempty"`
+}
+
+
+// Validate issue model.
+func (Issue) CheckValid(x *Issue) error {
+
+	if len(x.Title) < 5 {
+		return errors.New(" title must be at least 6 characters long")
+	}
+
+	if len(x.Description) < 49 {
+		return errors.New(" description must be at least 50 charactaers")
+	}
+
+	return nil
+
 }

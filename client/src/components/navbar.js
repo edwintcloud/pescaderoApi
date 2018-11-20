549295f0764bce548c63ec719b55456a098a4d5a
Edwin Cloud <ecloud412@gmail.com>
2018-11-20 15:33:01 -0800

    [add] Navbar to dashboard front-end

---
+++ client/src/components/navbar.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+  import axios from 'axios';
+  import { Link } from 'react-router-dom';
+
+class NavbarComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user : ''
+    }
+  }
+
+  componentWillMount(){
+    // get current logged in user
+    axios.get("http://localhost:5000/users/current", { withCredentials: true }).then(res => {
+      if(res.data.hasOwnProperty("_id")) {
+        this.setState({user:res.data})
+      } else {
+        window.location = '/';
+      }
+      console.log(res.data)
+    }).catch(err => {
+      window.location = '/';
+    })
+  }
+
+  logout() {
+    axios.post("http://localhost:5000/users/logout", { withCredentials: true }).then(res => {
+      window.location = "/";
+    }).catch(err => {
+      window.location = '/';
+    })
+  }
+
+  render() {
+
+    return (
+        <Navbar color="light" light expand="md">
+      <NavbarBrand href="/"></NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+          <Link to="/dashboard">
+            <NavLink>Dashboard</NavLink>
+            </Link>
+          </NavItem>
+          <NavItem>
+          <Link to="/dashboard/create-issue">
+            <NavLink>Create Issue</NavLink>
+            </Link>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              {this.state.user.firstName}
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>
+                My Issues
+              </DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem onClick={this.logout}>
+                Logout
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+    </Navbar>
+    )
+  }
+}
+
+export default NavbarComponent
--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import Map from '../components/map';
-import Axios from 'axios';
-
-class Dashboard extends Component {
+import NavbarComponent from '../components/navbar';
   
-  componentDidMount(){
-    // get current logged in user
-    Axios.get("http://localhost:5000/users/current", { withCredentials: true }).then(res => {
-      console.log(res.data)
-    })
-  }
+class Dashboard extends Component {
 
   render() {
     return (
+      <div style={{width:'100%'}}>
+        <NavbarComponent />
       <div className="dashboard_container">
         <Map></Map>
         {this.props.children}
+      </div>
       </div>
     )
   }

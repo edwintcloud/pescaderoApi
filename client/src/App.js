78259e9dd67c5b64cd270cb97fd48df427a4d711
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-28 15:36:59 -0800

    team page raw

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,19 @@ import Index from './pages/index';
 import Dashboard from './pages/dashboard';
 import SignUp from './pages/signup';
 import Login from './pages/login';
+import Team from './pages/team';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter> 
-      <div className="page_container">   
-         <Route exact path="/" component={Index} />
-         <Route exact path="/signup" component={SignUp} />
-         <Route exact path="/login" component={Login} />
-         <Route exact path="/dashboard" component={Dashboard} />
-      </div>
+      <BrowserRouter>
+        <div className="page_container">
+          <Route exact path="/" component={Index} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/team" component={Team} />
+        </div>
       </BrowserRouter>
     );
   }
---
+++ client/src/pages/team.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Team extends Component{
+
+
+    render(){
+        return(<div>
+            <h1>Join the team!</h1>
+            <p>Below this we will have requirements for different positions and roles.</p>
+        </div>)
+    }
+
+
+
+}
+
+
+
+export default Team;

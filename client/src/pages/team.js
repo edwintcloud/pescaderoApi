5554aa41e61f4d601a3a02426ab322e88ee37c6e
Carlos Alba <calba5141114@hackbookpro-2.local>
2018-11-29 12:08:56 -0800

    image service

--- client/src/pages/team.js
+++ client/src/pages/team.js
@@ -1,13 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import InputForm from '../components/inputform';
 
-class Team extends Component{
+
+class Team extends Component {
 
 
-    render(){
-        return(<div>
-            <h1>Join the team!</h1>
-            <p>Below this we will have requirements for different positions and roles.</p>
-        </div>)
+    render() {
+        return (
+            <div>
+                <h1>Join the team!</h1>
+                <p>Below this we will have requirements for different positions and roles.</p>
+                <InputForm />
+            </div>)
     }
 
 
